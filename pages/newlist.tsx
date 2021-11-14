a1970d17e164ead1aabdde54381c39f2736fe8a0
SolKul <46105598+SolKul@users.noreply.github.com>
2021-11-14 13:14:39 +0000

    update newlist

--- pages/newlist.tsx
+++ pages/newlist.tsx
@@ -1,34 +1,62 @@
 import firebaseApp from "../components/fire"
 import { getFirestore,collection, getDocs } from "firebase/firestore";
+import { getStorage, ref, getDownloadURL } from 'firebase/storage'
 import { useState,useEffect } from "react";
 
 import Layout from '../components/Layout'
 import { ImageList,ImageInfo } from "../components/ImageList";
 
 const db = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp)
+
+type FileInfo={
+  filePath:string
+  id:string
+}
 
 export default function Home(){
-  const [imlist, setImlist] = useState<ImageInfo[]>([])
+  const [imList, setImlist] = useState<ImageInfo[]>([])
 
   useEffect(()=>{
-    // 画像情報を格納するための仮の配列
-    const temp_imlist: ImageInfo[] = [];
+    // 順番通りに画像情報を格納するための配列
+    const fileInfoList:FileInfo[]=[]
+    // imlistにsetする前の画像情報を格納するための仮の配列
+    const tmpImList: ImageInfo[] = [];
     // collectionへの参照を取得
     const colRef = collection(db, "photos");
+    // Promiseの配列
+    const tasks: Array<any> = [];
     // collection内のdocumentの配列を取得
     getDocs(colRef).then((snapshot)=>{
-      // 配列の要素全てを取り出し、temp_imlistに格納
+      // 配列の要素全てを取り出し、file情報を順番通りにfileInfoListに格納
       snapshot.forEach((document)=>{
         const doc=document.data()
-        temp_imlist.push(
+        fileInfoList.push(
           {
-            id:document.id,
-            url:document.data().downloadUrl
+            filePath:doc.filePath,
+            id:document.id
           }
         )
       })
-      // 逆順にしてステートに保存
-      setImlist(temp_imlist.reverse())
+      
+      // 配列のmapではインデックス番号が取得できるので、
+      // 処理の順番が違っても順番通りにtmpImListに格納できる
+      // そしてそれらのPromiseをtasksに格納
+      fileInfoList.map((fileInfo,index)=>{
+        tasks.push(getDownloadURL(ref(storage,fileInfo.filePath))
+          .then((storageUrl:string)=>{
+            tmpImList[index]={
+              id:fileInfo.id,
+              url:storageUrl
+            }
+          })
+        )
+      })
+
+      // 全てのPromieseが終わったのを待ち、imListにsetする
+      Promise.all(tasks).then(() => {
+        setImlist(tmpImList)
+      })
     })
   },[])
 
@@ -36,7 +64,7 @@ export default function Home(){
     <div>
       <Layout header='Photo Sharing' title='New Photo List page'>
         こんにち！
-        <ImageList imlist={imlist}/>
+        <ImageList imlist={imList}/>
       </Layout>
     </div>
   )
