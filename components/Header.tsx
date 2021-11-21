77fd358ab5c03732b24925c7b556b23775201798
SolKul <46105598+SolKul@users.noreply.github.com>
2021-11-21 05:24:59 +0000

    update layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,19 @@
 import Link from 'next/link'
 
+import styles from '../styles/Home.module.scss'
+
 type Props={
   header:any
+  href?:string
 }
 
-export default function Header({header}:Props){
+export default function Header({header,href="/"}:Props){
   return (
     <div>
-      <Link href="/">
-        <a>
-        <div className="bg-primary px-3 text-white display-4">
+      <Link href={href}>
+        <div className={"bg-primary px-3 text-white display-4 "+styles.pointer}>
           <h1>{header}</h1>
         </div>
-        </a>
       </Link>
     </div>
   )
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,13 @@ import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
 
-export default function Layout({children,title,header}:any){
+export default function Layout({children,title,header,href="/"}:any){
   return (
     <div>
       <Head>
         <title>{title}</title>
       </Head>
-      <Header header={header} />
+      <Header header={header} href={href}/>
         {children}
       <Footer footer="This is footer" />
     </div>
