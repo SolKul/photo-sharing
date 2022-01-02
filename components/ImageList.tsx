f43e657e1a6ea8386916e7f1ebe4a4ac731286c4
SolKul <46105598+SolKul@users.noreply.github.com>
2022-01-02 15:35:13 +0000

    handle preview modal

--- components/ImageList.tsx
+++ components/ImageList.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+
 import Image from 'next/image'
 
 import styles from '../styles/Home.module.scss'
 
 export type ImageInfo = {
   id: string
-  url: string
+  thumbUrl: string
+  previewUrl:string
 }
 
 type ImageListProps = {
@@ -34,14 +37,24 @@ export const ImageIdList = ({ imlist }: ImageListProps) => {
 // ImageListコンポーネントはurlの配列を受け取って
 // li要素を生成し、instagram風に並べるコンポーネント
 export const ImageList = ({ imlist }: ImageListProps) => {
+  const [show, setShow] = useState<boolean>(false)
+  const [modalUrl, setModalUrl] = useState<string>("")
+
   // imlistプロパティの要素数が 0 であれば何も描画しない
   if (imlist.length == 0) return null;
+
+  const openModal =(previewUrl:string)=>{
+    setModalUrl(previewUrl)
+    setShow(true)
+  }
 
   // imlistからitemを取り出し、item.idとitem.urlを組み込んで
   // li要素配列を生成する。
   const listItems = imlist.map((item: ImageInfo) =>
     <div key={item.id} className="col-4 col-lg-3">
-      <div><Image src={item.url} objectFit="contain" layout="fill" alt="" /></div>
+      <div onClick={()=>openModal(item.previewUrl)}>
+        <Image src={item.thumbUrl} objectFit="contain" layout="fill" alt="" />
+      </div>
     </div>
   );
 
@@ -49,5 +62,35 @@ export const ImageList = ({ imlist }: ImageListProps) => {
     <div className={`row g-0 ${styles.square}`}>
       {listItems}
     </div>
+    <PreviewModal show={show} setShow={setShow} modalUrl={modalUrl}/>
   </div>
+}
+
+const PreviewModal=({show,setShow,modalUrl}:any)=>{
+  const closeModal = () => {
+    setShow(false)
+  }
+
+  if (show) {
+    return <div className={styles.modalOverlay} onClick={closeModal}>
+      <div className={`row align-items-center justify-content-center ${styles.modalContent}`}>
+      <div className="col-8" onClick={(e:any) => e.stopPropagation()}>
+        <div>
+        {
+          modalUrl!="" 
+          && 
+          <Image 
+            src={modalUrl} 
+            layout="fill" 
+            objectFit="contain" 
+            alt="" 
+          />
+        }
+        </div>
+      </div>
+      </div>
+    </div>
+  } else {
+    return null;
+  }
 }
